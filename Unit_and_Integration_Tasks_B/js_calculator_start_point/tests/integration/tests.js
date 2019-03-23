11fa5c1f120fbe68c1efd2b85145b55ab29b2358
Alexkcshing <alexkcshing@gmail.com>
2019-03-23 14:50:26 +0000

    pass output float test

--- Unit_and_Integration_Tasks_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Tasks_B/js_calculator_start_point/tests/integration/tests.js
@@ -10,13 +10,13 @@ describe('calculator functionality', function() {
   });
 
   // write integration tests here in the form of "it should do something..."
-  it('should have working number buttons', function(){
+  xit('should have working number buttons', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
-  it("should have working operator buttons", function(){
+  xit("should have working operator buttons", function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number1')).click();
     element(by.css('#number1')).click();
@@ -26,7 +26,7 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('20')
   })
 
-  it("should be able to chain multiple operations", function (){
+  xit("should be able to chain multiple operations", function (){
     running_total = element(by.css('#running_total'))
     element(by.css('#number5')).click();
     element(by.css('#operator_add')).click();
@@ -37,6 +37,25 @@ describe('calculator functionality', function() {
     element(by.css('#number8')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('22')
+  })
+
+  xit("should be able to output negative numbers", function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-8')
+  })
+
+  it("should be able to output decimal numbers", function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.be.within(0.333, 0.334)
+    // expect(running_total.getAttribute('value')).to.eventually.be.closeTo(0.333, 0.001)
   })
 
 });
