c20041d788a8849af759a1083e944facc100200a
Alexkcshing <alexkcshing@gmail.com>
2019-03-23 14:37:40 +0000

    pass integrations operation chain test

--- Unit_and_Integration_Tasks_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Tasks_B/js_calculator_start_point/tests/integration/tests.js
@@ -26,4 +26,17 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('20')
   })
 
+  it("should be able to chain multiple operations", function (){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('22')
+  })
+
 });
